d4e246771dc982cb300443c344d8a026aa74b76d
Julien cllt <collet.jln@gmail.com>
2022-03-01 18:25:47 +0100

    switch case

--- condition.php
+++ condition.php
@@ -1,12 +1,32 @@
 <?php
 // (int) indique que la valeur recu sera converti en integer (nombre)
-$note = (int)readline('entrez votre note : ');
-if ($note < 10) {
-    echo 'Vous n\'avez pas la moyenne dommage.';
-} elseif ($note === 10) {
-    echo 'Vous avez pile la moyenne.';
-} else {
-        echo 'Vous avez la moyenne bravo.';
-};
+//$note = (int)readline('entrez votre note : ');
+
+$action = (int)readline('Entrez votre action : (1: Attaquer, 2: Defendre, 3: Passer mon tour)');
+
+switch ($action) {
+    case 1:
+        echo 'J\'attaque';
+        break;
+    case 2:
+        echo 'Je defends';
+        break;
+    case 3:
+        echo 'Je ne fait rien';
+        break;
+    
+    default:
+        echo 'Commande invalide';
+        break;
+}
+
+
+// if ($note < 10) {
+//     echo 'Vous n\'avez pas la moyenne dommage.';
+// } elseif ($note === 10) {
+//     echo 'Vous avez pile la moyenne.';
+// } else {
+//         echo 'Vous avez la moyenne bravo.';
+// };
 
 ?>
