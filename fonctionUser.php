1c92f5a3f600ce60b1a8f354d887d6513630b585
Julien cllt <collet.jln@gmail.com>
2022-05-01 14:22:09 +0200

    fonction user2

--- fonctionUser.php
+++ fonctionUser.php
@@ -11,7 +11,7 @@ function bonjours ($name) {
 };
 
 
-// une fonction de base n'a pas accés a des variable a l'exterieur de celle-ci toute information externe dois etre passé en via des arguments.
+// une fonction de base n'a pas accés à des variables à l'exterieur de celle-ci toute information externe dois etre passé en via des arguments.
 function scopeVariable ($name = null) {
     //global permet d'avoir acces a une variable exterieur a la fonction. Méthode ne conseillé car le fonctionnement de la fonction depend de l'existance varible exterieur.
     global $lastname;
@@ -21,26 +21,62 @@ function scopeVariable ($name = null) {
     return "Bonjour $name $lastname \n";
 };
 
-function repondre_oui_non ($reponse) {
-    switch ($reponse) {
-        case 'o':
-            return true;
-            break;
-        case 'n':
-            return false;
-            break;
-        
-        default:
-            readline('voulez vous continuer');
-            break;
+function repondre_oui_non ($params) {
+
+    
+    while (true) {
+        $result = readline("$params");
+            switch ($result) {
+                case 'o':
+                    return true;
+                    break;
+                case 'n':
+                    return false;
+                    break;
+
+            }
     }
+
+  
 } ;
+
+//$result = repondre_oui_non('voulez vous continuer ? o/n : ');
+//var_dump($result);
+
+function demander_creneau ($phrase = 'Veuillez entre un creneau') {
+    echo $phrase . "\n";
+    while (true) {
+        $ouverture = (int)readline("Heure d'ouverture : "); 
+            
+            if ($ouverture >= 0 && $ouverture <= 23) {
+                break;
+            };
+    };
+
+    while (true) {
+        $fermeture = (int)readline('Heure de fermeture :');
+        
+            if ($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture) {
+                break;
+            }
+    };
+    
+    return [$ouverture, $fermeture];
+    
+
+};
+
+$creneau = demander_creneau();
+var_dump($creneau);
+
+
+//
 
 // bonjour();
 // $resultFunction = bonjours("Marie");
 // echo $resultFunction;
- $resultFunction = scopeVariable('John');
- echo $resultFunction;
+//  $resultFunction = scopeVariable('John');
+//  echo $resultFunction;
 
 
 ?>
